aa7eb8ee826bb9500bb3d967a4b9af15b615e306
20jun01 <10junjun20@gmail.com>
2023-01-06 12:27:37 +0900

    ✨ impl text animation on AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="about">
-    <h1>ら</h1>
-    <h1>ん</h1>
-    <h1>だ</h1>
-    <h1>む</h1>
-    <h1>飲</h1>
-    <h1>食</h1>
-    <h1>店</h1>
+  <div class="TextAnime1">
+    <p v-for="(t, index) in text" :key="index" class="item" :style="{ animationDelay: index * 100 + 'ms' }" v-text="t"
+      colors="purple darken-1"></p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: 'らんだむ飲食店'
+    }
+  }
+}
+</script>
+
+<style scoped>
+@keyframes text-in {
+  0% {
+    transform: translate(0, -20px);
+    opacity: 0;
+  }
+}
+
+.item {
+  display: inline-block;
+  min-width: 0.3em;
+  font-size: 2rem;
+  animation: text-in .8s cubic-bezier(0.22, 0.15, 0.25, 1.43) 0s backwards;
+  color: lightseagreen;
+}
+</style>
